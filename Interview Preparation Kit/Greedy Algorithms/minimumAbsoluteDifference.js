98be192c9c98e2bce2957da30917c3f53c14174a
David Garcia <david.garcia.nete@gmail.com>
2020-02-19 03:26:43 -0500

    Debug max abs diff

--- Interview Preparation Kit/Greedy Algorithms/minimumAbsoluteDifference.js
+++ Interview Preparation Kit/Greedy Algorithms/minimumAbsoluteDifference.js
@@ -1,26 +1,15 @@
-function createPairs(arr){
-    var pairArray = [];
-    for(var i=0;i<arr.length;i++){
-        for(var j = 0; j<arr.length;j++){
-            if(i!=j)pairArray.push({'x':arr[i], 'y':arr[j]});
-        }
-    }
-return pairArray;
-}
-
 // Complete the minimumAbsoluteDifference function below.
 function minimumAbsoluteDifference(arr) {
     var min = Number.MAX_VALUE;
-    var pairArray = createPairs(arr);
-    for(var value of pairArray){
-        var diff = value['x']-value['y'];
+    arr.sort();
+    for(var i=0;i<arr.length-1;i++){
+        var diff = arr[i]-arr[i+1];
         var abs = Math.abs(diff);
-        if(abs<min)min = abs;
+        if(abs<min) min = abs;
     }
-    console.log(min);
+   console.log(min);
 }
 
-var arr=[3, -7, 0];
-
+var arr=[636651840, 845593958, 514430275, 950990774]
 minimumAbsoluteDifference(arr);
 
