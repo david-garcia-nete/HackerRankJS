00f443d6d28277461a46306ad3f5c5b29bff7b96
David Garcia <david.garcia.nete@gmail.com>
2020-03-12 05:12:26 -0400

    Save progress

--- Interview Preparation Kit/Stacks and Queues/balancedBrackets.js
+++ Interview Preparation Kit/Stacks and Queues/balancedBrackets.js
@@ -28,6 +28,43 @@ function isBalanced(s) {
             }
         }
     }
+    
+    /*
+     * I want to cover the case where there may be an open bracket on the end and it may just
+     * get pushed onto the stack.  We can iterate backwards through the string checking for balance again
+     * reversing the direction of the brackets.
+     * 
+     * We expect to start with closing brackets and end with matching open brackets.  If this is not
+     * the case we return NO.  Otherwise both checks have passed and we return YES.  
+     */
+    var stack = [];
+    for(var i = s.length-1; i>=0; i--){
+        if(s[i]==')' || s[i]==']' || s[i]=='}') stack.push(s[i]);
+        if(s[i]=='('){
+            if(stack[stack.length-1]==')'){
+                stack.pop();
+                continue;
+            }else{
+                return 'NO';
+            }
+        }
+        if(s[i]=='['){
+            if(stack[stack.length-1]==']'){
+                stack.pop();
+                continue;
+            }else{
+                return 'NO';
+            }
+        }
+        if(s[i]=='{'){
+            if(stack[stack.length-1]=='}'){
+                stack.pop();
+                continue;
+            }else{
+                return 'NO';
+            }
+        }
+    }
     return 'YES'
 }
 
